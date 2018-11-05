3a91b5ca4b94363e6d6329db56547ccc66d25f0b
Surendra Reddy <surendrarv9@gmail.com>
2018-11-05 22:50:02 +0530

    Done. 11'O Clock.

---
+++ ProBookTutorials/FunWithClasses/NewCar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithClasses
+{
+    class NewCar
+    {
+        public string carName;
+        public int speed;
+
+        public NewCar(string name, int speed)
+        {
+            carName = name;
+            this.speed = speed;
+        }
+    }
+}
---
+++ ProBookTutorials/FunWithClasses/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int speed = 50;
+            string name = "Tesla 2020";
+
+            NewCar car = new NewCar(name, speed);
+
+            car.speed = 200;
+
+            Console.WriteLine($"Name: {car.carName} Speed: {car.speed} ");
+        }
+    }
+}
