02baf7412881ea531548a80c1461444680486674
Surendra Reddy <surendrarv9@gmail.com>
2019-01-05 15:30:50 +0530

    5th Jan 2018

--- ProBookTutorials/ListExample/Program.cs
+++ ProBookTutorials/ListExample/Program.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ListExampleEntity;
 
 namespace ListExample
 {
-    class Program
+    public class Program
     {
+        static List<Person> lstPerson = new List<Person>();
+        static string name;
+        static string age;
+        static string city;
         static void Main(string[] args)
         {
+            for(int i = 1; i < 3; i++)
+            {
+                Console.WriteLine($"{i} Person.\n");
+                Console.WriteLine($"Name: ");
+                name = Convert.ToString(Console.ReadLine());
+                Console.WriteLine($"Age: ");
+                age = Convert.ToString(Console.ReadLine());
+                Console.WriteLine($"City: ");
+                city = Convert.ToString(Console.ReadLine());
+                lstPerson.Add(new Person() { Name = name, Age = age, City = city});
+            }
+            GetPersonDetails(lstPerson);
+        }
+
+        static void GetPersonDetails(List<Person> personObject)
+        {
+            Console.WriteLine("Name\tAge\tCity");
+            foreach (Person item in lstPerson)
+            {
+                Console.WriteLine($"{item.Name}\t{item.Age}\t{item.City}");
+            }
         }
     }
 }
---
+++ ProBookTutorials/ListExampleEntity/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExampleEntity
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string City { get; set; }
+    }
+}
