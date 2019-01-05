31120b95f9e9800b4e5bead635be6c487558b878
Surendra Reddy <surendrarv9@gmail.com>
2019-01-05 14:42:40 +0530

    5th Jan Sync.

---
+++ ProBookTutorials/ArrayList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ArrayListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+
+            arrayList.AddRange(new string[] { "Hello", "One", "Two"});
+
+            Console.WriteLine($"The Length of the Array is {arrayList.Count}");
+
+            arrayList.AddRange(new int[] { 1, 2, 3, 4});
+
+            Console.WriteLine($"The Length of the Array is {arrayList.Count}");
+
+            arrayList.Add(true);
+
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
